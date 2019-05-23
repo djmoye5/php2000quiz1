72a6f6f90681cd50889e0399c3ba804a00bb5fcd
Denerius Moye <dmoye@techlaunch.io>
2019-05-23 17:30:39 -0400

    revision1

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -6,6 +6,19 @@ class ProfileController extends Controller
 {
 	public function indexAction()
 	{
-		
+
+	}
+
+	public function submitAction()
+	{
+		$name = $this->request->get('name');
+		$dob = $this->request->get('dob');
+		$bio = $this->get('bio');
+		$message = $this->request->get('message');
+
+		$this->view->name = $name;
+		$this->view->dob = $dob;
+		$this->view->bio = $bio;
+		$this->view->message = $message;
 	}
 }
